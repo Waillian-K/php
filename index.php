d6b66164fd54aa6b0bd618d975c43a7d81323b39
Waillian20 <waillianchit.loyar@gmail.com>
2025-05-19 15:12:24 +0630

    into php

--- index.php
+++ index.php
@@ -1,31 +1,79 @@
 <?php
-    echo "Hello World";
-    echo "<br>"
-    $name = 
+  echo "Hello World";
+  echo "<br>";
+  $name = "John";
+  echo "Hello $name";
+  // variable types
+  // string, integer, float, boolean, array
+  $age = 20;
+  echo "<br>";
+  echo "Hello $name, you are $age years old";
+
+  $height = 1.75;
+  echo "<br>";
+  echo "Hello $name, you are $age years old and $height meters tall";
+
+  $is_married = false;
+  echo "<br>";
+  echo "Hello $name, you are $age years old and $height meters tall and $is_married";
+
+  $hobbies = array("Coding", "Sleeping", "Eating");
+  echo "<br>";
+  echo "Hello $name, you are $age years old and $height meters tall and $is_married and your hobbies are $hobbies[0], $hobbies[1], and $hobbies[2]";
+
+  // control flow
+  // if, else, elseif, switch
+  if($age < 18) {
+    echo "<br>";
+    echo "You are not allowed to vote";
+  } else {
+    echo "<br>";
+    echo "You are allowed to vote";
+  }
+
+  // loops
+  // while, do while, for
+  while($age < 18) {
+    echo "<br>";
+    echo "You are not allowed to vote";
+    $age++;
+  }
+
+  for($i = 0; $i < 5; $i++) {
+    echo "<br>";
     echo "Hello $name";
+  }
+
+  // functions
+  // function name($parameter) {
+  //   // code
+  // }
+  function sayHello($name) {
     echo "<br>";
-    
-    //variable types
-    //string , intrger, float,boolean , array
-    $age = 20;
-    echo "Hello $name, you are $age years old";
+    echo "Hello $name";
+  }
+  sayHello("John");
 
-    $height = 1.75;
-    echo "<br>";
-    echo "Hello $name, you are $age years old and $height meters tall";
-
-    $is_married = false;
-    echo "<br>"
-    echo "Hello $name, you are $age years old and $height meters tall and $is_married";
-
-    $hobbies = array("coding","sleeping","eating");
-    echo "<br>"
-    echo "Hello $name, you are $age years old and $height meters tall and $is_married and  my hobbies are $hobbies[0],$hobbies[1],$hobbies[2]";
+  // php statements
+  // variable and types
+  // control flow
+  // loops
+  // functions
 
 
-    //control flow
-    //if , else , elseself , switch
-    
+  // Swap two variables using a temporary third variable.
+  $a = 10;
+  $b = 20;
+  $temp = $a; // temp = 10
+  $a = $b; // a = 20
+  $b = $temp; // b = 10
+  echo $a;
+  echo $b;
 
-
+  // Check data types using var_dump()
+  var_dump($name); // value and type
+  var_dump($age);
+  var_dump($height);
+  var_dump($is_married);
+  var_dump($hobbies);
 ?>
