0038f9e59885cbe5f4f91db99bd2235f92a6d6b6
Waillian20 <waillianchit.loyar@gmail.com>
2025-05-19 14:18:49 +0630

    first commit

---
+++ index.php
@@ -0,0 +1,31 @@
+<?php
+    echo "Hello World";
+    echo "<br>"
+    $name = 
+    echo "Hello $name";
+    echo "<br>";
+    
+    //variable types
+    //string , intrger, float,boolean , array
+    $age = 20;
+    echo "Hello $name, you are $age years old";
+
+    $height = 1.75;
+    echo "<br>";
+    echo "Hello $name, you are $age years old and $height meters tall";
+
+    $is_married = false;
+    echo "<br>"
+    echo "Hello $name, you are $age years old and $height meters tall and $is_married";
+
+    $hobbies = array("coding","sleeping","eating");
+    echo "<br>"
+    echo "Hello $name, you are $age years old and $height meters tall and $is_married and  my hobbies are $hobbies[0],$hobbies[1],$hobbies[2]";
+
+
+    //control flow
+    //if , else , elseself , switch
+    
+
+
+?>
